9cef10a960ffd6f9ed362a8b29a61519eb7262de
LLEEVV2020 <lleevv2020@gmail.com>
2022-12-07 20:02:53 +0400

    add

--- src/js/main.js
+++ src/js/main.js
@@ -40,7 +40,35 @@ function getElement(url, selector, c) {
 
   }
 
-
-
     //VizovStart();
+
+
+// ИНПУТ С одной цифрой
+    let input_cifri = document.querySelectorAll('.cifri-input');
+
+    if(input_cifri !== null){
+
+        for (let input of input_cifri) {
+
+            input.addEventListener('keydown', function(event) {
+                // Разрешаем: backspace, delete, tab и escape
+                if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 ||
+                    // Разрешаем: Ctrl+A
+                    (event.keyCode == 65 && event.ctrlKey === true) ||
+                    // Разрешаем: home, end, влево, вправо
+                    (event.keyCode >= 35 && event.keyCode <= 39)) {
+
+                    // Ничего не делаем
+                    return;
+                } else {
+                    // Запрещаем все, кроме цифр на основной клавиатуре, а так же Num-клавиатуре
+                    if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+                        event.preventDefault();
+                    }
+                }
+            });
+
+        }
+
+    }
 
